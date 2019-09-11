7c25e093e64f3d4834fe6cc2f87f77b9a9902cfa
Prince-Amir <prince.ahmadi@gmail.com>
2019-09-11 12:44:36 +0430

    Update ProxCHK.sh

--- ProxCHK.sh
+++ ProxCHK.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 echo "Enter Path to portscan resualt: "
 read POSCPath
-POSCPath = cat POSCPath
+POSCPath="$(while read line; do echo $line; done < $POSCPath)"
+
 for n in $POSCPath
 do
     echo "Working on $n file name now"
-    OUTPUT="$(curl -Is --proxy 104.144.218.121:3128 http://askubuntu.com | head -1)"
+    OUTPUT="$(curl -Is --proxy $n http://askubuntu.com | head -1)"
     #echo "${OUTPUT}"
     if [[ "${OUTPUT}" = *"HTTP/1.1 403 Forbidden"* ]]
     then
